c3e6178882dbdc1c3a1d57bf830d210250ddf4f7
Jill Martin <47822285+jillmartinstudent@users.noreply.github.com>
2019-11-06 13:52:21 +0000

    .

--- CourseworkMarkOutputScreen.php
+++ CourseworkMarkOutputScreen.php
@@ -4,7 +4,6 @@ require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php')
 
 <!DOCTYPE html>
 
-<!DOCTYPE html>
 
 <html>
 <!--   
@@ -17,67 +16,6 @@ mysql_select_db("databasename",$conn);
   <meta name="viewport" content="width=device-width, initial-scale=1">
  
     
-
-<style>
-body {
-  font-family: "Lato", sans-serif;
-}
-
-    .button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-.button2 {background-color: #008CBA;} /* Blue */
-.button3 {background-color: #f44336;} /* Red */ 
-.button4 {background-color: #e7e7e7; color: black;} /* Gray */ 
-.button5 {background-color: #555555;} /* Black */
-
-
-.sidenav {
-  height: 100%;
-  width: 160px;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.sidenav a {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
-
-.main {
-  margin-left: 160px; /* Same as the width of the sidenav */
-  font-size: 28px; /* Increased text to enable scrolling */
-  padding: 0px 10px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-</style>
-
 
 <div class="sidenav">
   <a href="#about">About</a>
--- StudentSelectionScreen.php
+++ StudentSelectionScreen.php
@@ -2,6 +2,7 @@
 require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php')
    -->
 
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -19,69 +20,7 @@ require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php')
   <meta name="viewport" content="width=device-width">
 
     
-<style>
-body
 
-{
-  font-family: "Lato", sans-serif;
-}
-
-    .button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-.button2 {background-color: #008CBA;} /* Blue */
-.button3 {background-color: #f44336;} /* Red */ 
-.button4 {background-color: #e7e7e7; color: black;} /* Gray */ 
-.button5 {background-color: #555555;} /* Black */
-
-.sidenav {
-  height: 100%;
-  width: 250px;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.sidenav a {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
-
-.main {
-  margin-left: 250px; /* Same as the width of the sidenav */
-  font-size: 24px; /* Inc
-                   reased text to enable scrolling */
-  padding: 0px 20px;
-}
-
-@media screen and (max-height: 250px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-
-
-</style>
 <div id="qunit"></div>
   <div id="qunit-fixture"></div>
   <script src="https://code.jquery.com/qunit/qunit-2.9.2.js"></script>
