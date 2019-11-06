d69330145716570faf997c9f791b2bc92bc58662
Mark Stephenson <32237489+MarkAStephenson@users.noreply.github.com>
2019-11-06 13:20:04 +0000

    Added David's db connection

--- view_coursework.php
+++ view_coursework.php
@@ -5,39 +5,29 @@
 <h2>View marks for coursework</h2>
 Select from pre-defined option in the field below
 <p>
-    
-<!--?php
-require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
-?-->
+<?php    
+	
+    //include db connection details, can't link using full www path as I think it's disabled for security reasons
+	//using path on disk instead
+	require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
+			
+	//some test stuff
+	$courses = "SELECT courseworkId, title FROM sc_coursework";
+    $result = mysqli_query($connection, $courses);
+	$numberOfRows = mysqli_affected_rows($connection);
+	
+	print('Number of rows returned is: ' . $numberOfRows);
 
-<?php
-//Connect to our MySQL database using the PDO extension.
-//$pdo = new PDO('mysql:host=localhost;dbname=agile', 'root', '');
-require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
+    mysqli_close($connection);
+	
+?>
 
-$pdo = new PDO($connection);
-
-//Our select statement. This will retrieve the data that we want.
-$sql = "SELECT courseworkId, title FROM coursework";
-
-//Prepare the select statement.
-$stmt = $pdo->prepare($sql);
-//$stmt = $connection->prepare($sql);
- 
-//Execute the statement.
-$stmt->execute();
- 
-//Retrieve the rows using fetchAll.
-$courses = $stmt->fetchAll();
- ?>
 <form action="return_courseware.php" method="post">
 <select>
-    <?php foreach($courses as $course): ?>
+    <?php foreach($result as $course): ?>
         <option value="<?= $course['courseworkId']; ?>"><?= $course['title']; ?></option>
     <?php endforeach; ?>
  
 </select>
 <input type="submit">
 </form-->
-</body>
-</html>
