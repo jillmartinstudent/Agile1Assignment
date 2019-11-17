9dcb469667531fec38a6f41eff075d6eddecbb3f
dytegry <42341882+dytegry@users.noreply.github.com>
2019-11-17 18:03:35 +0000

    Added dummy pages and stoped error on index

---
+++ edit_coursework.php
@@ -0,0 +1,29 @@
+<?php
+	
+	require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
+	
+?>
+
+<!DOCTYPE html>
+<html>
+
+
+<head>
+    <title>
+        Student Coursework Management portal
+    </title>
+    <link href="index.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+    <script src="main.js"></script>
+
+</head>
+
+<body id="document">
+    <h1 id="title">Edit coursework</h1>
+	
+	<p>Under construction</p>
+    
+</body>
+
+
+</html>
