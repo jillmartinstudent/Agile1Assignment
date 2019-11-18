fe5f590061961354db6291919b8cf20672d5f1dc
Jill Martin <47822285+jillmartinstudent@users.noreply.github.com>
2019-11-18 20:04:11 +0000

    Not my changes so just a sync

--- convert_final_marks.php
+++ convert_final_marks.php
@@ -7,70 +7,62 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 </head>
 <body>
+    
 <!--Calculation to convert all student final marks and output in the Alpha Numeric Scale-->
-<?php
-	
-    //test php to check 
-	
-    //include db connection details, can't link using full www path as I think it's disabled for security reasons
-    //using path on disk instead
-    //db connection
-    require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
-
-     $_POST["value"];
-
-?>
-
 
 <h2> Convert all student final marks into the Alpha Numeric Scale </h2>
 
-<?php
-//Take the StuMatric from studentSelectionScreen, calculate the number of submissions for that student, then sum the results.
-
-
-$total=0; 
-$submissions = "SELECT submissionId FROM sc_submissions WHERE stuMatric = $valueToSearch";
-$query = mysql_query($submissions);
-while ($row = mysql_fetch_assoc($query)) {
-echo "{$row['Submission: ']}<br />";
-$total += $row['Submission'];
-}
-echo "Total = $total";
-        
-?>
-
-<br> <br> 
-
-    
-<?php
-//divide the sum of the results by the number of submissions (avg Mark) and round to 0 decimal places
-        
-if ($query -> num_rows > 0) {
-    $avgMark = $total / $submissions;
-    echo ("The average mark is: (round($avgMark,0))");
-}
-
-    mysqli_close($connection);
-	
-?>
-
-<br> <br> 
+Student Matriculation Number: <?php echo $_POST["studentID"]; ?> <br><br>
 
 <?php
-// lookup the number from the sc_markingscheme table.
 
-$grade = "SELECT markingScale FROM sc_markingscheme WHERE percentage".$avgMark;
-$query1 = mysql_query($grade);
-$final;
+require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
 
-if ($query1->num_rows > 0) {
-    //output data of each row
-    $row = $query1->fetch_assoc();
-    $final = $row['markingscale'];
-} else {
-    echo "No results available";
-    
+//Take the StuMatric from studentSelectionScreen, calculate the number of submissions for that student
+
+$submissions = "SELECT s.mark, s.submittedDate, s.mitigatingReq, s.mitigatingUph, s.secondSub, c.title
+FROM sc_submissions s LEFT OUTER JOIN sc_coursework c
+ON s.courseworkId = c.courseworkId
+WHERE stuMatric = '" . $_POST["studentID"] . "'";
+$result = mysqli_query($connection, $submissions);
+$numberOfRows = mysqli_affected_rows($connection);
+
+if ($result-> num_rows > 0) {
+        echo "The number of submissions for this student is: $numberOfRows <br> <br>";
+    } else {
+    echo "0 results";
+    }
+
+//get total marks for all submissions and create average mark
+
+$marks = "SELECT SUM(mark)as total_mark FROM sc_submissions WHERE stuMatric = '" . $_POST["studentID"] . "'";
+$sumResult = mysqli_query($connection, $marks);
+$row = mysqli_fetch_assoc($sumResult);
+$total = $row["total_mark"];
+$avMark = round($total/$numberOfRows);
+
+if ($sumResult-> num_rows > 0) {
+        echo "The average mark for this student is: $avMark <br> <br>";
+    } else {
+    echo "0 results";
 }
+
+//Lookup number from the sc_markingscheme table
+
+$scale = "SELECT markingScale FROM sc_markingscheme WHERE percentage = '$avMark'";
+$output = mysqli_query($connection, $scale);
+$rows = mysqli_fetch_assoc($output);
+$endResult = mysqli_affected_rows($connection);
+
+if ($output-> num_rows > 0) {
+        echo "Marking Scale Conversion: $endResult";
+    } else {
+    echo "0 results";
+}
+
+mysqli_close($connection);
 
 ?>
 
+</body>
+</html>
--- coursework_output.php
+++ coursework_output.php
@@ -59,12 +59,49 @@
                     <th>Date Submitted</th>
                 </tr>
 <?php
+
 //get coursework marks as per selection
 $test = "SELECT a.stuMatric AS 'matric', mark, submittedDate, s.firstName AS 'stufirst', s.secondName AS 'stusecond'
         FROM sc_submissions a
         LEFT OUTER JOIN sc_students s ON a.stuMatric = s.stuMatric
         WHERE courseworkId = '" . $_POST["value"] . "'";
-$result = mysqli_query($connection, $test);
+
+    //test php to check 
+    //Taken from view coursework user story
+    //include db connection details, can't link using full www path as I think it's disabled for security reasons
+	//using path on disk instead
+	require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
+    
+    //hardcoded post variable for coursework selection
+    //$_POST["value"] = 1;
+
+    $test = "SELECT c.title AS 'coursetitle', m.title AS 'moduletitle', l.title AS 'lectitle', l.firstName as 'lecfirst', l.secondName AS 'lecsecond'
+    FROM sc_coursework c
+    LEFT OUTER JOIN sc_modules m ON c.moduleId = m.moduleId
+    LEFT OUTER JOIN sc_lecturers l ON m.lecturerId = l.lecturerId
+    WHERE c.courseworkId = '" . $_POST["value"] . "'";
+    $result = mysqli_query($connection, $test);
+    $row = mysqli_fetch_assoc($result);
+    $cTitle = $row["coursetitle"] ;
+    $mTitle = $row["moduletitle"];
+    $lec = $row["lectitle"] . " " . $row["lecfirst"] . " " . $row["lecsecond"];
+    $numberOfRows = mysqli_affected_rows($connection);       
+        
+        
+        
+        
+	//some test stuff
+
+
+   echo "<h2>Coursework: </h2><p>" . $cTitle . "</p>";
+   echo "<h2>Module: </h2><p>" . $mTitle . "</p>";
+   echo "<h2>Lecturer : </h2><p>" . $lec . "</p>";
+
+  
+    //used for testing
+    //$numberOfRows = mysqli_affected_rows($connection);
+        
+    //print('Number of rows returned is: ' . $numberOfRows);
 
 //populate table with data
 while ($row = mysqli_fetch_assoc($result)) {
