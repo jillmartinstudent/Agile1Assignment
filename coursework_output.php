744409151b647fc47bbe816056b7849a66f783c3
Jill Martin <47822285+jillmartinstudent@users.noreply.github.com>
2019-11-18 20:11:45 +0000

    Update coursework_output.php

--- coursework_output.php
+++ coursework_output.php
@@ -65,44 +65,8 @@ $test = "SELECT a.stuMatric AS 'matric', mark, submittedDate, s.firstName AS 'st
         FROM sc_submissions a
         LEFT OUTER JOIN sc_students s ON a.stuMatric = s.stuMatric
         WHERE courseworkId = '" . $_POST["value"] . "'";
-
-    //test php to check 
-    //Taken from view coursework user story
-    //include db connection details, can't link using full www path as I think it's disabled for security reasons
-	//using path on disk instead
-	require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
-    
-    //hardcoded post variable for coursework selection
-    //$_POST["value"] = 1;
-
-    $test = "SELECT c.title AS 'coursetitle', m.title AS 'moduletitle', l.title AS 'lectitle', l.firstName as 'lecfirst', l.secondName AS 'lecsecond'
-    FROM sc_coursework c
-    LEFT OUTER JOIN sc_modules m ON c.moduleId = m.moduleId
-    LEFT OUTER JOIN sc_lecturers l ON m.lecturerId = l.lecturerId
-    WHERE c.courseworkId = '" . $_POST["value"] . "'";
     $result = mysqli_query($connection, $test);
-    $row = mysqli_fetch_assoc($result);
-    $cTitle = $row["coursetitle"] ;
-    $mTitle = $row["moduletitle"];
-    $lec = $row["lectitle"] . " " . $row["lecfirst"] . " " . $row["lecsecond"];
-    $numberOfRows = mysqli_affected_rows($connection);       
         
-        
-        
-        
-	//some test stuff
-
-
-   echo "<h2>Coursework: </h2><p>" . $cTitle . "</p>";
-   echo "<h2>Module: </h2><p>" . $mTitle . "</p>";
-   echo "<h2>Lecturer : </h2><p>" . $lec . "</p>";
-
-  
-    //used for testing
-    //$numberOfRows = mysqli_affected_rows($connection);
-        
-    //print('Number of rows returned is: ' . $numberOfRows);
-
 //populate table with data
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<tr><td>" . $row["matric"] . "</td>";
