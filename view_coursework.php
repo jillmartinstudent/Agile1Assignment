9d57dd1bc7f4f25ba086264daad94c86298bb9b8
gj62 <gajennings@dundee.ac.uk>
2019-11-18 21:17:01 +0000

    Update view_coursework.php

--- view_coursework.php
+++ view_coursework.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>View marks for coursework</title>   
+        <link href="index.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+        <script src="main.js"></script>
+    </head>
+    <body>
+
+        <h2> View marks for coursework </h2>
+
 <?php  
     
     //include db connection details, can't link using full www path as I think it's disabled for security reasons
@@ -9,5 +21,26 @@
 	$numberOfRows = mysqli_affected_rows($connection);
 
     mysqli_close($connection);
-    echo $numberOfRows;
     ?>
+        
+        
+<!-- Query the coursework table within the database to populate the dropdown field -->
+<form action="coursework_output.php" method="post">
+<select name="value">
+    
+    <?php
+    foreach($result as $course):
+    ?>
+    
+    <option value="<?=$course['courseworkId']; ?>"> <?=$course['title'];?></option>
+    <?php endforeach; ?>
+    
+    
+</select>
+<button type="submit" name="submit"> Submit </button>
+
+</form>
+
+    </body>
+    
+</html>
