9f8564aed7b126201aed44e90b3c2185c3e4c75e
Jill Martin <47822285+jillmartinstudent@users.noreply.github.com>
2019-11-18 20:32:24 +0000

    Update student_selection.php

--- student_selection.php
+++ student_selection.php
@@ -7,11 +7,11 @@
         <script src="main.js"></script>
     </head>
     <body>
-<!-- Taken from 'View marks for specific coursework' user story-->
+        <!-- Taken from 'View marks for specific coursework' user story-->
         <h1>Search for Students:</h1>
         Select from pre-defined option in the field below
         <p>
-<?php
+            <?php
 //include db connection details, can't link using full www path as I think it's disabled for security reasons
 //using path on disk instead
             require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
@@ -22,7 +22,7 @@
             $numberOfRows = mysqli_affected_rows($connection);
 
             mysqli_close($connection);
-?>
+            ?>
         <form action="convert_final_marks.php" method="post">
             <select name="value">
                 <?php foreach ($result as $mark): ?>
