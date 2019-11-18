dfb59804ff0c2d14dc9ba2c7e6ba8ba5757b6046
Jill Martin <47822285+jillmartinstudent@users.noreply.github.com>
2019-11-18 20:16:15 +0000

    sync caused issues - corrected

--- landing_page.php
+++ landing_page.php
@@ -25,17 +25,7 @@ require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
     <header>
         <nav id="main-navigation">
 
-            <ul>
-                <li><a href="index.php">Return to Login Page</a></li>
-                <li><a>Register Student</a></li>
-                <li><a>View Student</a></li>
-                <li><a href="view_coursework.php">View Coursework</a></li>
-                <li><a>Create Coursework</a></li>
-                <li><a>Edit Coursework</a></li>
-                <li><a>Add Marks</a></li>
-                <li><a href="view_average_mark.php">Display Average</a></li>
-                <li><a href="student_selection.php">Display Final Alphanumeric Grade</a></li>
-            </ul>
+
 
         <ul>
                     <li><a href="register_student.php">* Register Student</a></li>
