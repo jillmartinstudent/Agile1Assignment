0cf7a114e08a2549c71ca3c4189e3177dd5e954e
Mark Stephenson <32237489+MarkAStephenson@users.noreply.github.com>
2019-11-18 19:08:04 +0000

    Create view_coursework.php
    empty file

--- view_coursework.php
+++ view_coursework.php
@@ -1,49 +1,8 @@
-<!DOCTYPE html>
+<?php
 
-<html>
-    <head>
-    <title>View marks for coursework</title>   
-    
-    <link href="index.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
-    <script src="main.js"></script>
-    </head>
-<body>
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-
-<h2>View marks for coursework</h2>
-Select from pre-defined option in the field below
-<p>
-<?php    
-	
-//include db connection details, can't link using full www path as I think it's disabled for security reasons
-//using path on disk instead
-
-// Taken from 'View marks for specific coursework' user story
-    require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
-      ?>
-    <?php
-    $courses = "SELECT courseworkId, title FROM sc_coursework";
-
-    $result = mysqli_query($connection, $courses);
-    $numberOfRows = mysqli_affected_rows($connection);
-
-    mysqli_close($connection);
-	
-    ?>
-
-<!-- Query the database for all available coursework titles and generate a drop-down list from the results. -->
-
-<form action="coursework_output.php" method="post">
-<select name="value">
-    <?php foreach($result as $course): ?>
-    <option value="<?= $course['courseworkId']; ?>"><?= $course['title']; ?></option>
-    <?php endforeach; ?>
- 
-    
-</select>
-<button type="submit" name="submit" >Submit</button>
-</form>
-
-</body>
-</html>
