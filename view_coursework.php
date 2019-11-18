d5e2fb30dc47b644cf24b23759e58721e8a33e59
Mark Stephenson <32237489+MarkAStephenson@users.noreply.github.com>
2019-11-18 20:26:06 +0000

    Update view_coursework.php
    Created DB connection and queried table to test for 5 rows

--- view_coursework.php
+++ view_coursework.php
@@ -6,3 +6,14 @@
  * and open the template in the editor.
  */
 
+    //include db connection details, can't link using full www path as I think it's disabled for security reasons
+	//using path on disk instead
+	require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
+        
+        	$courses = "SELECT courseworkId, title FROM sc_coursework";
+    $result = mysqli_query($connection, $courses);
+	$numberOfRows = mysqli_affected_rows($connection);
+
+    mysqli_close($connection);
+    echo $numberOfRows;
+    ?>
