f97796aa249b361d8adfdd9201c3d3643f211aaa
Mark Stephenson <32237489+MarkAStephenson@users.noreply.github.com>
2019-12-04 13:44:27 +0000

    Update view_coursework.php
    Modified to allow for the selection of modules

--- view_coursework.php
+++ view_coursework.php
@@ -9,20 +9,28 @@
     <body>
 
         <h2> View marks for coursework </h2>
-        <p>Select from one of the pre-defined options in the drop down field below</p>
+        <p>Select a module from one of the pre-defined options in the drop down field below</p>
 
-<?php  
-    
-    //include db connection details, can't link using full www path as I think it's disabled for security reasons
+            <?php  
+        //include db connection details, can't link using full www path as I think it's disabled for security reasons
 	//using path on disk instead
 	require_once ('c:/websites/2018-ga/davidgrayland/agile/_php/dbconnect.php');
-        
+     
+/* CODE FROM SPRINT 1
         	$courses = "SELECT courseworkId, title FROM sc_coursework";
     $result = mysqli_query($connection, $courses);
 	$numberOfRows = mysqli_affected_rows($connection);
-
-    mysqli_close($connection);
-    ?>
+         * 
+         *  mysqli_close($connection);
+         */
+        
+        
+//SPRINT 2 CODE
+        // https://www.youtube.com/watch?v=TNPxG2yrPlM
+            $modules = "SELECT moduleId, title FROM sc_modules";
+            $result = mysqli_query($connection, $modules);  
+           $numberOfRows = mysqli_affected_rows($connection);
+       ?>
         
         
 <!-- Query the coursework table within the database to populate the drop down field -->
@@ -30,10 +38,10 @@
 <select name="value">
     
     <?php
-    foreach($result as $course):
+    foreach($result as $module):
     ?>
     
-    <option value="<?=$course['courseworkId']; ?>"> <?=$course['title'];?></option>
+    <option value="<?=$module['moduleId']; ?>"> <?=$module['title'];?></option>
     <?php endforeach; ?>
     
     
