ab4565704638b4f085500da63ef91d80a1d75b1e
gj62 <gajennings@dundee.ac.uk>
2019-12-06 11:11:39 +0000

    Changes

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 //Form login validation code
 
-function IsEmpty() {
+function isEmpty() {
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (email == "" || password == "") {
+        alert("Please complete both login fields!");
+        return false;
+    } else if (email = str.includes("@")) {
+        return true;
+    } else {
         window.open("landing_page.php");
     }
+}
