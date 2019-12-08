a0d12d81f0b0d21c0404ce6ef905853e8f06f7a6
dytegry <42341882+dytegry@users.noreply.github.com>
2019-12-08 15:24:13 +0000

    Update to show marks for all submissions and fix for terminology in create course

--- return_average_mark.php
+++ return_average_mark.php
@@ -8,71 +8,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Student Coursework Management - Return Average Mark</title>
+        <title>Student Coursework Management - Student's Marks</title>
         <link href="index.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
         <script src="main.js"></script>
     </head>
     <body>
         <?php
-            //get number of submissions details for selected student
-            $sql = "SELECT s.mark, s.submittedDate, s.mitigatingReq, s.mitigatingUph, s.secondSub, c.title
-            FROM sc_submissions s LEFT OUTER JOIN sc_coursework c
-            ON s.courseworkId = c.courseworkId
-            WHERE stuMatric = '" . $_POST["value"] . "'
-            ORDER BY s.submittedDate ";
-            $result = mysqli_query($connection, $sql);
-            $numberOfRows = mysqli_affected_rows($connection);
-
-            //get total marks for all submissions and create average mark
-            $sql = "SELECT SUM(mark) as total_mark FROM sc_submissions WHERE stuMatric = '" . $_POST["value"] . "'";
-            $sumResult = mysqli_query($connection, $sql);
-            $row = mysqli_fetch_assoc($sumResult);
-            $total = $row["total_mark"];
-            
-            //avoid division of 0
-            if ($numberOfRows <> 0) {
-                $avMark = round($total/$numberOfRows);
-                } else {
-                    $avMark = 0;
-                }
-            
             //get student name
             $sql = "SELECT firstName, secondName FROM sc_students WHERE stuMatric = '" . $_POST["value"] . "'";
             $nameResult = mysqli_query($connection, $sql);
             $row = mysqli_fetch_assoc($nameResult);
             $fullName = $row["firstName"] . " " . $row["secondName"];
 
-            //populate page with details of student's average mark, marking scale and results
-            if ($numberOfRows <> 0) {
-                //get the scale and details for average mark
-                $sql = "SELECT DISTINCT s.markingScale AS 'scale', aggScale, descript, honoursClass
-                FROM sc_markingscheme s LEFT OUTER JOIN sc_markingdescript m
-                ON s.markingScale = m.markingScale
-                WHERE s.percentage = '" . $avMark . "'";
-        
-                $markResult = mysqli_query($connection, $sql);
-                $alpharow = mysqli_fetch_assoc($markResult);
-                $scale = $alpharow["scale"];
-                $aggScale = $alpharow["aggScale"];
-                $desc = $alpharow["descript"];
-                $honours = $alpharow["honoursClass"];
-
-                echo "<h1>The average mark for " . $fullName . " (" . $_POST["value"] . ") " . " is " . $avMark . "%</h1>";
-                echo "<p><b>" . $fullName . "'s (" . $_POST["value"] . ") final grade is: " . $scale . "</b></p>";
-                echo "<p>This is considered " . $desc . " and is/would be a " . $honours . " on the honours scale, with a aggregated scale of " . $aggScale . ".</p>";
-        
-            } else {
-                //for students that haven't submitted yet
-                echo "<h1>The average mark for " . $fullName . " (" . $_POST["value"] . ") " . " is " . $avMark . "%</h1>";
-                echo "<p><b>" . $fullName . " (" . $_POST["value"] . ") has not completed any submissions yet.</b></p><br>";
-            }
+            echo "<h1>Displaying marks for " . $fullName . " (" . $_POST["value"] . ") " . "</h1>";
             
         ?>
-        <h2>Breakdown of marks:</h2>
-        <table id="allMarks">
+        <h2>Breakdown of marks for year 1:</h2>
+        <table id="1Marks">
             <tr>
+                <th>Module</th>
                 <th>Coursework</th>
+                <th>Coursework Item</th>
                 <th>Mark</th>
                 <th>Date Submitted</th>
                 <th>Mitigating Requested</th>
@@ -80,24 +37,154 @@
                 <th>Second Submission</th>
             </tr>
             <?php
+                //get marks for year 1 submissions
+                $sql ="SELECT s.mark, s.submittedDate, s.mitigatingReq, s.mitigatingUph, s.secondSub, c.title AS 'itemtitle', w.title AS 'cwtitle', m.title AS 'moduletitle'
+                FROM sc_submissions s 
+                LEFT OUTER JOIN sc_courseworkItem c ON s.courseworkItemId = c.courseworkItemId
+                LEFT OUTER JOIN sc_coursework w ON c.courseworkId = w.courseworkId
+                LEFT OUTER JOIN sc_modules m ON w.moduleId = m.moduleId
+                LEFT OUTER JOIN sc_modulestocourses t ON m.moduleId = t.moduleId
+                WHERE stuMatric = '" . $_POST["value"] . "' AND t.yr = '1'
+                GROUP BY s.submissionId
+                ORDER BY m.title, w.title";
+                $yr1marks = mysqli_query($connection, $sql);;
 
-            //populate table with data
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr><td>" . $row["title"] .  "</td>";
-                echo "<td>" . $row["mark"] .  "</td>";
-                echo "<td>" . $row["submittedDate"] .  "</td>";
-                echo "<td>" . $row["mitigatingReq"] .  "</td>";
-                echo "<td>" . $row["mitigatingUph"] .  "</td>";
-                echo "<td>" . $row["secondSub"] .  "</td></tr>";
-            }
+                //populate table with data
+                while ($row = mysqli_fetch_assoc($yr1marks)) {
+                    echo "<tr><td>" . $row["moduletitle"] .  "</td>";
+                    echo "<td>" . $row["cwtitle"] .  "</td>";
+                    echo "<td>" . $row["itemtitle"] .  "</td>";
+                    echo "<td>" . $row["mark"] .  "</td>";
+                    echo "<td>" . $row["submittedDate"] .  "</td>";
+                    echo "<td>" . $row["mitigatingReq"] .  "</td>";
+                    echo "<td>" . $row["mitigatingUph"] .  "</td>";
+                    echo "<td>" . $row["secondSub"] .  "</td></tr>";
+                }
+            ?>
+        </table>
+        </br>
+        <h2>Breakdown of marks for year 2:</h2>
+        <table id="2Marks">
+            <tr>
+                <th>Module</th>
+                <th>Coursework</th>
+                <th>Coursework Item</th>
+                <th>Mark</th>
+                <th>Date Submitted</th>
+                <th>Mitigating Requested</th>
+                <th>Mitigating Upheld</th>
+                <th>Second Submission</th>
+            </tr>
+            <?php
+                //get marks for year 2 submissions
+                $sql ="SELECT s.mark, s.submittedDate, s.mitigatingReq, s.mitigatingUph, s.secondSub, c.title AS 'itemtitle', w.title AS 'cwtitle', m.title AS 'moduletitle'
+                FROM sc_submissions s 
+                LEFT OUTER JOIN sc_courseworkItem c ON s.courseworkItemId = c.courseworkItemId
+                LEFT OUTER JOIN sc_coursework w ON c.courseworkId = w.courseworkId
+                LEFT OUTER JOIN sc_modules m ON w.moduleId = m.moduleId
+                LEFT OUTER JOIN sc_modulestocourses t ON m.moduleId = t.moduleId
+                WHERE stuMatric = '" . $_POST["value"] . "' AND t.yr = '2'
+                GROUP BY s.submissionId
+                ORDER BY m.title, w.title";
+                $yr1marks = mysqli_query($connection, $sql);;
 
+                //populate table with data
+                while ($row = mysqli_fetch_assoc($yr1marks)) {
+                    echo "<tr><td>" . $row["moduletitle"] .  "</td>";
+                    echo "<td>" . $row["cwtitle"] .  "</td>";
+                    echo "<td>" . $row["itemtitle"] .  "</td>";
+                    echo "<td>" . $row["mark"] .  "</td>";
+                    echo "<td>" . $row["submittedDate"] .  "</td>";
+                    echo "<td>" . $row["mitigatingReq"] .  "</td>";
+                    echo "<td>" . $row["mitigatingUph"] .  "</td>";
+                    echo "<td>" . $row["secondSub"] .  "</td></tr>";
+                }
+            ?>
+        </table>
+        </br>
+        <h2>Breakdown of marks for year 3:</h2>
+        <table id="3Marks">
+            <tr>
+                <th>Module</th>
+                <th>Coursework</th>
+                <th>Coursework Item</th>
+                <th>Mark</th>
+                <th>Date Submitted</th>
+                <th>Mitigating Requested</th>
+                <th>Mitigating Upheld</th>
+                <th>Second Submission</th>
+            </tr>
+            <?php
+                //get marks for year 2 submissions
+                $sql ="SELECT s.mark, s.submittedDate, s.mitigatingReq, s.mitigatingUph, s.secondSub, c.title AS 'itemtitle', w.title AS 'cwtitle', m.title AS 'moduletitle'
+                FROM sc_submissions s 
+                LEFT OUTER JOIN sc_courseworkItem c ON s.courseworkItemId = c.courseworkItemId
+                LEFT OUTER JOIN sc_coursework w ON c.courseworkId = w.courseworkId
+                LEFT OUTER JOIN sc_modules m ON w.moduleId = m.moduleId
+                LEFT OUTER JOIN sc_modulestocourses t ON m.moduleId = t.moduleId
+                WHERE stuMatric = '" . $_POST["value"] . "' AND t.yr = '3'
+                GROUP BY s.submissionId
+                ORDER BY m.title, w.title";
+                $yr1marks = mysqli_query($connection, $sql);;
+
+                //populate table with data
+                while ($row = mysqli_fetch_assoc($yr1marks)) {
+                    echo "<tr><td>" . $row["moduletitle"] .  "</td>";
+                    echo "<td>" . $row["cwtitle"] .  "</td>";
+                    echo "<td>" . $row["itemtitle"] .  "</td>";
+                    echo "<td>" . $row["mark"] .  "</td>";
+                    echo "<td>" . $row["submittedDate"] .  "</td>";
+                    echo "<td>" . $row["mitigatingReq"] .  "</td>";
+                    echo "<td>" . $row["mitigatingUph"] .  "</td>";
+                    echo "<td>" . $row["secondSub"] .  "</td></tr>";
+                }
+            ?>
+        </table></br>
+        <h2>Breakdown of marks for year 4:</h2>
+        <table id="4Marks">
+            <tr>
+                <th>Module</th>
+                <th>Coursework</th>
+                <th>Coursework Item</th>
+                <th>Mark</th>
+                <th>Date Submitted</th>
+                <th>Mitigating Requested</th>
+                <th>Mitigating Upheld</th>
+                <th>Second Submission</th>
+            </tr>
+            <?php
+                //get marks for year 2 submissions
+                $sql ="SELECT s.mark, s.submittedDate, s.mitigatingReq, s.mitigatingUph, s.secondSub, c.title AS 'itemtitle', w.title AS 'cwtitle', m.title AS 'moduletitle'
+                FROM sc_submissions s 
+                LEFT OUTER JOIN sc_courseworkItem c ON s.courseworkItemId = c.courseworkItemId
+                LEFT OUTER JOIN sc_coursework w ON c.courseworkId = w.courseworkId
+                LEFT OUTER JOIN sc_modules m ON w.moduleId = m.moduleId
+                LEFT OUTER JOIN sc_modulestocourses t ON m.moduleId = t.moduleId
+                WHERE stuMatric = '" . $_POST["value"] . "' AND t.yr = '4'
+                GROUP BY s.submissionId
+                ORDER BY m.title, w.title";
+                $yr1marks = mysqli_query($connection, $sql);;
+
+                //populate table with data
+                while ($row = mysqli_fetch_assoc($yr1marks)) {
+                    echo "<tr><td>" . $row["moduletitle"] .  "</td>";
+                    echo "<td>" . $row["cwtitle"] .  "</td>";
+                    echo "<td>" . $row["itemtitle"] .  "</td>";
+                    echo "<td>" . $row["mark"] .  "</td>";
+                    echo "<td>" . $row["submittedDate"] .  "</td>";
+                    echo "<td>" . $row["mitigatingReq"] .  "</td>";
+                    echo "<td>" . $row["mitigatingUph"] .  "</td>";
+                    echo "<td>" . $row["secondSub"] .  "</td></tr>";
+                }
+            ?>
+        </table>
+        <?php
+            
             //free memory
-            mysqli_free_result($result);
+            mysqli_free_result($yr1marks);
 
             //close connection
             mysqli_close($connection);
-
-            ?>
-        </table>
+        ?>
     </body>
 </html> 
